9ea704c6d6fae19dd7c8ceeeb6d56626f1cb6e25
Sumanth Murali <sumanthm@ufl.edu>
2017-04-03 19:43:56 -0400

    interfacing node with mongo

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const MongoClient = require('mongodb').MongoClient
+var bodyParser = require('body-parser');
 var db;
 
+var jsonParser = bodyParser.json();
+app.use(jsonParser);
 
 MongoClient.connect('mongodb://sumanth1019:sumanth@ds149030.mlab.com:49030/drbeacon', (err, database) => {
   
@@ -14,11 +17,53 @@ MongoClient.connect('mongodb://sumanth1019:sumanth@ds149030.mlab.com:49030/drbea
 
 })
 
+	app.get('/', (req, res) => {
+	
 
-app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray(function(err, results) {
-  console.log(results)
-  // send HTML file populated with quotes here
-})
 
-});
+	});
+
+	app.post('/user_profile_update', (req, res) => {
+		db.collection('User_Profile').save(req.body);
+	});
+
+	app.post('/check_username', (req, res) => {
+		db.collection('User_Profile').count({"_id": req.body._id}, function(err, count) {
+		    if(count == 0)
+		    {
+		    	
+		    }
+		    else
+		    {
+		    	
+		    }
+		});
+	});
+
+
+	// Basic 404 Page
+	app.use((req, res, next) => {
+		var err = {
+			stack: {},
+			status: 404,
+			message: "Error 404: Page Not Found '" + req.path + "'"
+		};
+
+		// Pass the error to the error handler below
+		next(err);
+	});
+
+	// Error handler
+	app.use((err, req, res, next) => {
+		console.log("Error found: ", err);
+		res.status(err.status || 500);
+
+		//res.render('error', {title: 'Error', error: err.message});
+	});
+	// ------------------------------------------------------------------------
+
+	// Handle killing the server
+	process.on('SIGINT', () => {
+		internals.stop();
+		process.kill(process.pid);
+	});
